ef8b8f3684d85baf5c7a55665a1dc2d8fda073c6
Aramayis Orkusyan <aramayisorkusyan@gmail.com>
2020-09-20 16:10:35 -0700

    Add smooth scrolling to sections on nav click

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    // This object listens to scroll events and dispatches event handlers as needed.
     let ScrollSpy = {
         init: function() {
             this.cacheDOM();
@@ -26,5 +27,58 @@ $(document).ready(function() {
         }
     };
 
+    // This listens for click events on the nav links and scrolls the page to
+    // the corresponding section. 
+    let NavScroller = {
+        init: function() {
+            this.cacheDOM();
+            this.attachEventListeners();
+        },
+
+        cacheDOM: function() {
+            this.header   = document.querySelector('header')
+            this.hero     = document.querySelector('.hero')
+            this.main     = document.querySelector('main')
+            this.social   = document.querySelector('.social')
+            this.navLinks = document.querySelectorAll('.nav-link')
+        },
+
+        attachEventListeners: function() {
+            this.header.addEventListener('click', this.onNavLinkClicked.bind(this))
+        },
+
+        onNavLinkClicked: function(event) {
+            event.preventDefault();
+            let link = event.target;
+            let section = this.linkToDOMSection(link);
+            let yOffset = section.offsetTop;
+            // We need to subtract the height of the header from the yOffset
+            // because the header has fixed positioning. Without this adjustment,
+            // the header will cover part of the top of the section.
+            yOffset -= this.header.offsetHeight;
+            window.scrollTo({
+                left: 0,
+                top: yOffset,
+                behavior: 'smooth'
+            });
+        },
+
+        linkToDOMSection: function(link) {
+            let innerHTML = link.innerHTML.toLowerCase();
+
+            if (innerHTML.search('home') > -1) {
+                return this.hero;
+            } else if (innerHTML.search('about') > -1) {
+                return this.main;
+            } else if (innerHTML.search('contact') > -1) {
+                return this.social;
+            }
+
+            return null;
+        }
+
+    };
+
     ScrollSpy.init();
+    NavScroller.init();
 })
